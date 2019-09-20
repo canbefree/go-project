9d0f2a7f24bcab07587058b5b758e8722fd0da53
xieyt01 <xieyt01@mingyuanming.com>
2019-09-20 09:40:17 +0000

    提交

--- main.go
+++ main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"net/http"
-
+	"database/sql"
 	"github.com/emicklei/go-restful"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
 	// "github.com/emicklei/go-restful-swagger12"
 )
 
 func main() {
-	ws := new(restful.WebService)
-	ws.Route(ws.GET("/").
-		Doc("get all users").
-		Writes("").
-		Returns(200, "OK", "ok"))
-
-	container := restful.NewContainer().Add(ws)
-	http.ListenAndServe(":8080", container)
+	//模拟sql进程池
+	sql := sql.Open("mysql", "root:123456@(mariadb)/go_project")
 }
